9bdf9382f0b1a7a5117e32de11588ac49e440d56
Vlada Shevchenko <vlada.shevchenko.it@gmail.com>
2024-04-05 12:47:58 +0300

    clean_text func

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import re, os
 from collections import Counter
 
+
+def clean_text(text: str):
+    """
+    Cleans the text from punctuation marks except spaces.
+
+    Parameters:
+    text (str): The input text to be cleaned.
+
+    Returns:
+    str: The cleaned text.
+    """
+    cleaned_text = re.sub(r'[^\w\s]', ' ', text).lower()
+    return cleaned_text
+
+
 def get_output_filename():
     """
     Returns the path to the input file.
