cdb86970fa42430cb3bcae9c023dec16d525efb2
Vlada Shevchenko <vlada.shevchenko.it@gmail.com>
2024-04-05 13:02:52 +0300

    added sample_text fixture

---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+import os
+import pytest
+from main import clean_text, read_text, find_top_words, write_to_file, get_output_filename, get_input_filename
+
+
+@pytest.fixture
+def sample_text():
+    return "Hello Vlada. Sing songs. stay strong. Vlada. I live in Kyiv, Ukraine. Stay strong. Hello Harry. Vlada. Stay. stay here. stay."
+
+
